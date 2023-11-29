57ff9e754e0d863e2745e7b04091236c444b6c73
yllsallahu <ys68687@ubt-uni.net>
2023-11-29 22:41:34 +0100

    fixed header in signup page
    fixed margins in signup page

---
+++ js.js
@@ -0,0 +1,9 @@
+let nameRegex = /^[A-Z][a-z]{3,8}$/;
+let emailRegex =/[a-zA-Z.-_]+@+[a-z]+\.+[a-z]{2-3}$/;
+
+function validateForm(){
+    let fnamegetelementi = document.getElementsByClassName('fname');
+    let fnameerror = document.getElementsByClassName('fname.error');
+   
+
+}
