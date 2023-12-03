8aa85ef5d4b62aa47b37c18dc1a65fd2d125a4c1
yllsallahu <ys68687@ubt-uni.net>
2023-12-03 18:29:52 +0100

    Fixed sign-up button
    fixed the size of the web

--- js.js
+++
@@ -1,9 +0,0 @@
-let nameRegex = /^[A-Z][a-z]{3,8}$/;
-let emailRegex =/[a-zA-Z.-_]+@+[a-z]+\.+[a-z]{2-3}$/;
-
-function validateForm(){
-    let fnamegetelementi = document.getElementsByClassName('fname');
-    let fnameerror = document.getElementsByClassName('fname.error');
-   
-
-}
